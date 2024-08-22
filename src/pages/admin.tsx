d45ad76f1287fb7ed527e4ca1533c826da90b676
jihyeok04 <dong4757@t3q.com>
2024-08-22 18:41:06 +0900

    login, admin 디자인 수정 및 기능 추가 (localStorage를 사용한 로그인, 로그아웃, 회원탈퇴 기능)

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,13 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from '@/components/layout'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { Title } from '@/components/ui/Title'
 
 const Admin = () => {
+  const [userEmail, setUserEmail] = useState<string | null>(null)
+  const [userName, setUserName] = useState<string | null>(null)
+  const router = useRouter()
+
+  useEffect(() => {
+    setUserEmail(localStorage.getItem('userEmail'))
+    setUserName(localStorage.getItem(`${userEmail}.name`))
+  }, [])
+
+  const withdrawAccount = () => {
+    localStorage.removeItem(`${userEmail}.name`)
+    localStorage.removeItem(`${userEmail}.email`)
+    localStorage.removeItem(`${userEmail}.password`)
+    localStorage.removeItem(`${userEmail}.address`)
+    localStorage.removeItem('userEmail')
+    localStorage.removeItem('userName')
+    alert('회원 탈퇴되었습니다.')
+    router.push('/login')
+  }
+
   return (
     <Layout>
-      <div className="flex flex-col items-start w-full mt-32 ml-6">
-        <h1>회원 페이지</h1>
-        <Link href="/" className="flex justify-center items-center mt-4 w-16 h-8 border">Logout</Link>
+      <div className="flex flex-col items-start w-full mt-32 ml-12">
+        <Title>마이 페이지</Title>
+        <p className="ml-1 font-semibold">{userName}님 환영합니다!</p>
+        <div className="flex items-center justify-center mt-4">
+          <Link
+            href="/"
+            className="flex justify-center items-centerw-16 p-2 border rounded"
+            onClick={() => {
+              localStorage.removeItem('userEmail')
+              localStorage.removeItem('userName')
+            }}
+          >
+            로그아웃
+          </Link>
+          <button
+            className="ml-4 text-red-500 text-sm"
+            type="button"
+            onClick={withdrawAccount}
+          >
+            회원 탈퇴하기
+          </button>
+        </div>
       </div>
     </Layout>
   )
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,35 +1,79 @@
 import React from 'react'
 import Layout from '@/components/layout'
 import { useRouter } from 'next/router'
+import Link from 'next/link'
+import styles from './../../styles/RegisterPage.module.css'
+import { Title } from '@/components/ui/Title'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { LoginFormType } from '@/types/type'
 
 const Login = () => {
+  const methods = useForm<LoginFormType>()
   const router = useRouter()
-  const linkToAdminPage = () => {
-    router.push('/admin')
+
+  const onSubmit: SubmitHandler<LoginFormType> = (data) => {
+    if (
+      localStorage.getItem(`${data.email}.email`) == data.email &&
+      localStorage.getItem(`${data.email}.password`) == data.password
+    ) {
+      localStorage.setItem('userEmail', data.email)
+      router.push('/admin')
+    } else {
+      alert(
+        '일치하는 계정이 존재하지 않습니다.\n이메일 또는 비밀번호를 다시 확인해주세요.'
+      )
+    }
   }
 
   return (
     <Layout>
-      <div className="flex flex-row justify-center items-center w-2/3 h-48 mt-48 mx-24 border border-gray-200">
-        <div className="flex flex-col w-3/5 h-1/2 gap-3">
-          <input
-            className="pl-4 h-full border border-gray-200"
-            placeholder="ID"
-          />
-          <input
-            className="pl-4 h-full border border-gray-200"
-            type="password"
-            placeholder="Password"
-          />
-        </div>
+      <div className={styles.pageLayout}>
+        <Title>로그인</Title>
+        <div className={styles.formLayout}>
+          <form onSubmit={methods.handleSubmit(onSubmit)}>
+            <div className={styles.formGroup}>
+              <label htmlFor="email" className="mt-2">
+                이메일
+              </label>
+              <input
+                id="email"
+                type="text"
+                className="mt-2 p-1.5 border w-full rounded"
+                {...methods.register('email', {
+                  required: '이메일을 입력해주세요',
+                })}
+              />
+              {methods.formState.errors.email && (
+                <p className={styles.formSchemaError}>
+                  {methods.formState.errors.email.message}
+                </p>
+              )}
+            </div>
 
-        <button
-          className="flex justify-center items-center ml-4 w-1/3 h-2/5 bg-rose-300/50 font-bold text-gray-100"
-          type="button"
-          onClick={linkToAdminPage}
-        >
-          Login
-        </button>
+            <div className={styles.formGroup}>
+              <label htmlFor="password">비밀번호</label>
+              <input
+                id="password"
+                type="password"
+                className="mt-2 p-1.5 border w-full rounded"
+                {...methods.register('password', {
+                  required: '비밀번호를 입력해주세요',
+                })}
+              />
+              {methods.formState.errors.password && (
+                <p className={styles.formSchemaError}>
+                  {methods.formState.errors.password.message}
+                </p>
+              )}
+            </div>
+
+            <button className={styles.submitButton}>로그인</button>
+            <Link href="/register-page" className={styles.linkText}>
+              아직 회원이 아니신가요?&nbsp;
+              <span className='underline'>회원가입하기</span>
+            </Link>
+          </form>
+        </div>
       </div>
     </Layout>
   )
