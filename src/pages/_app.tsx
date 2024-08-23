79460f2ea45c66270ca553e15cae1fc7fbef4121
jihyeok04 <dong4757@t3q.com>
2024-08-23 17:24:19 +0900

    uesQuery hooks가 정상적으로 동작할 수 있도록 루트 컴포넌트에 QueryClient, QueryClientProvider추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
-import { AppProps } from 'next/app';
+import { AppProps } from 'next/app'
 import '../../styles/globals.css'
 import dotenv from 'dotenv'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   dotenv.config()
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
