1adcce7aedd2fed8e9456384ebf2e2a56f84526a
jihyeok04 <dong4757@t3q.com>
2024-08-23 17:21:08 +0900

    svg컴포넌트를 사용하기 위해서 webpack번들러를 설치, 설정 파일의 필요한 코드 추가

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,17 @@ const nextConfig = {
   images: {
     domains: ['221.158.142.142'],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
 }
 
 export default nextConfig
