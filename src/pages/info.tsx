f4e19b5c373a32f209859fad8667166fb23275ae
jihyeok04 <dong4757@t3q.com>
2024-08-23 15:06:15 +0900

    api호출 방식 변경으로 더이상 사용하지 않는 Axios컴포넌트 대신 바로 식물 리스트 컴포넌트를 출력

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import AxiosComponent from '../components/Axios'
 import Layout from '@/components/layout'
 import ScrollBtn from '@/components/ScrollBtn'
+import ItemList from '@/components/ItemList'
 
 const Info = () => {
   return (
     <Layout>
-      <AxiosComponent />
+      <ItemList />
       <ScrollBtn />
     </Layout>
   )
