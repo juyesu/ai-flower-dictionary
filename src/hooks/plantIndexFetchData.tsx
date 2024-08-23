a9d1616357cfa2090917822031531b708be8dffd
jihyeok04 <dong4757@t3q.com>
2024-08-23 16:15:49 +0900

    axios요청시 path와 params를 분리하여 전달하도록 변경

--- src/hooks/plantIndexFetchData.tsx
+++ src/hooks/plantIndexFetchData.tsx
@@ -4,11 +4,21 @@ import axios from 'axios'
 const GARDEN_API_URL =
   'https://apis.data.go.kr/B554620/gardenPrntInfoService/getGardenPrntInfoList?serviceKey=sL7tbaOTpeVYTaIXr22d8usE%2FX0BGm8MmIKamsm%2B%2BqYtrxj%2BPgUqop7AJuJlUjhiEsJzYzdRHjRkXgAnVI7TeA%3D%3D&pageNo=1&numOfRows=100&type=json'
 
+const GARDEN_API_PATH =
+  'https://apis.data.go.kr/B554620/gardenPrntInfoService/getGardenPrntInfoList'
+
 export const plantIndexFetchData = () => {
+  const params = {
+    serviceKey:
+      'sL7tbaOTpeVYTaIXr22d8usE/X0BGm8MmIKamsm++qYtrxj+PgUqop7AJuJlUjhiEsJzYzdRHjRkXgAnVI7TeA==',
+    pageNo: 1,
+    numOfRows: 100,
+    type: 'json',
+  }
   return useQuery({
-    queryKey: [`GET ${GARDEN_API_URL}`, GARDEN_API_URL],
+    queryKey: [`GET ${GARDEN_API_PATH}`, params],
     queryFn: async () => {
-        return await axios.get(GARDEN_API_URL)
-    }
-})
+      return await axios.get(GARDEN_API_PATH, { params })
+    },
+  })
 }
