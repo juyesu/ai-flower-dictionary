48b7a2f87e6a1fafad36c288c0430799f6788fbe
juyesu <dong4757@naver.com>
2025-01-23 16:22:38 +0900

    _app.tsx에서 Provider함수로 하위 컴포넌트를 감싸서 앱에 전역적으로 ThemeProvider가 적용되도록 변경

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { AppProps } from 'next/app'
 import '../../styles/globals.css'
 import dotenv from 'dotenv'
+import Providers from '@/components/ThemeProvider'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const queryClient = new QueryClient()
@@ -10,7 +11,9 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   dotenv.config()
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <Providers>
+        <Component {...pageProps} />
+      </Providers>
     </QueryClientProvider>
   )
 }
