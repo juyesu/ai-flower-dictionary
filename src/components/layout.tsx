512bbfa55c5fbe098589fe21d8a214ac7569d161
juyesu <dong4757@naver.com>
2025-01-26 20:45:24 +0900

    Layout footer의 margin top을 줄이고, 높이를 소폭 증가

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -101,14 +101,14 @@ const RootLayout = ({ children }: ChildrenComponentsProps) => {
 
           {children}
 
-          <div className="mt-52 flex flex-col justify-center items-center h-32 w-full border-t ">
+          <div className="mt-28 flex flex-col justify-center items-center h-40 w-full border-t">
             <p className="w-full text-center text-sm font-semibold text-rose-400">
               Made by 유한대학교 인공지능학과 인공지능과 언어 4조
             </p>
             <p className="mt-1.5 w-full text-center text-xs font-semibold text-gray-400">
               김지혁 양서연 이유찬 채기석 최지혜 추연우
             </p>
-            <p className="mt-3 w-full text-center text-xs text-gray-400">
+            <p className="mt-4 w-full text-center text-xs text-gray-400">
               powerd by Chat GPT , Teachable Machine
             </p>
           </div>
