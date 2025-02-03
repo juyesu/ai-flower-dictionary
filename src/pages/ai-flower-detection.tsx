48a9f95bd3cc5b9331f98aa8381b59237345daa0
juyesu <dong4757@naver.com>
2025-02-03 20:54:37 +0900

    webcamModel컴포넌트를 호출하는 페이지를 기존 index에서 새로운 ai-flower-detection로 교체

---
+++ src/pages/ai-flower-detection.tsx
@@ -0,0 +1,14 @@
+import Layout from '@/components/layout'
+import ScrollBtn from '@/components/ScrollBtn'
+import WebcamModel from '@/components/WebcamModel'
+
+const AiFlowerDetection = () => {
+  return (
+    <Layout>
+      <WebcamModel />
+      <ScrollBtn />
+    </Layout>
+  )
+}
+
+export default AiFlowerDetection
