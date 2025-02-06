89b48e2d0a00624ee624ec9d58d44d9ca0ff46ba
juyesu <dong4757@naver.com>
2025-02-06 17:08:13 +0900

    plant fetch data 호출 로직 개선 (함수 분리, 에러 핸들링, 재시도 및 지연설정)

--- src/hooks/plantIndexFetchData.tsx
+++ src/hooks/plantIndexFetchData.tsx
@@ -1,19 +1,29 @@
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
+import { PlantIndexParams } from '@/types/type'
+
+const fetchPlantIndexData = async (params: PlantIndexParams) => {
+  const response = await axios.get(process.env.NEXT_PUBLIC_GARDEN_API_PATH, {
+    params,
+  })
+  return response.data
+}
 
 export const plantIndexFetchData = () => {
-  const params = {
+  const params: PlantIndexParams = {
     serviceKey: process.env.NEXT_PUBLIC_GARDEN_API_KEY,
     pageNo: 1,
     numOfRows: 100,
     type: 'json',
   }
+
   return useQuery({
     queryKey: [`GET ${process.env.NEXT_PUBLIC_GARDEN_API_PATH}`, params],
-    queryFn: async () => {
-      return await axios.get(process.env.NEXT_PUBLIC_GARDEN_API_PATH, {
-        params,
-      })
-    },
+    queryFn: () => fetchPlantIndexData(params),
+    retry: (failureCount, error) =>
+      axios.isAxiosError(error) && error.response
+        ? failureCount < 3 && error.response.status !== 400
+        : false,
+    retryDelay: (retryAttempt) => Math.min(10000 * retryAttempt, 30000),
   })
 }
