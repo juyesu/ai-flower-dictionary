74400cc1e14cdbe0862b9941be6189b72927a2c7
juyesu <dong4757@naver.com>
2025-02-22 17:08:59 +0900

    아직 수정되지 않았던 import경로 절대 경로로 추가 수정

--- src/components/ai-flower-dection/AiFlowerDetection.tsx
+++ src/components/ai-flower-dection/AiFlowerDetection.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { useState } from 'react'
-import styles from './../../styles/ItemList.module.css'
+import styles from '@styles/ItemList.module.css'
 import WebcamModel from './WebcamModel'
 import FileUploadModel from './FileUploadModel'
 
