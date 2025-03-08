1f5c4df08ff89f2bea410f430b0cd520af67940d
juyesu <dong4757@naver.com>
2025-03-08 22:51:20 +0900

    MyDictionary 페이지 추가

---
+++ src/pages/my-dictionary.tsx
@@ -0,0 +1,14 @@
+import Layout from '@/components/common/layout'
+import ScrollBtn from '@/components/common/ScrollBtn'
+import MyDictionaryPage from '@/components/my-dictionary/MyDictionaryPage'
+
+const MyDictionary = () => {
+  return (
+    <Layout>
+      <MyDictionaryPage />
+      <ScrollBtn />
+    </Layout>
+  )
+}
+
+export default MyDictionary
