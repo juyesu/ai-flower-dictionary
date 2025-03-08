00c41d7285c318b0a3970c0eb9c3db38c6dfb426
juyesu <dong4757@naver.com>
2025-03-08 23:15:33 +0900

    model이 중복으로 로드될 경우에 발생하는 에러 수정

--- src/components/ai-flower-dection/WebcamModel.tsx
+++ src/components/ai-flower-dection/WebcamModel.tsx
@@ -25,14 +25,15 @@ const WebcamModel = ({ AIErrorModalOpen }: AIModelProps) => {
   useEffect(() => {
     const loadModel = async () => {
       try {
-        if (!model) {
-          const loadedModel = await tmImage.load(
-            '/plants_detection_model/model.json',
-            '/plants_detection_model/metadata.json'
-          )
-          setModel(loadedModel)
-          setMaxPredictions(loadedModel.getTotalClasses())
-        }
+        if (model) return
+
+        const loadedModel = await tmImage.load(
+          '/plants_detection_model/model.json',
+          '/plants_detection_model/metadata.json'
+        )
+
+        setModel(loadedModel)
+        setMaxPredictions(loadedModel.getTotalClasses())
       } catch (modelError) {
         console.error('Error loading model:', modelError)
         if (!isLoading && (!data || modelError || error)) {
