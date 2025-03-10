2dd5d324cbbfbc106e83bb8479d961501794a228
juyesu <dong4757@naver.com>
2025-03-10 18:19:38 +0900

    svg외부에 button태그 추가, 접근성 개선

--- src/components/common/ScrollButton.tsx
+++ src/components/common/ScrollButton.tsx
@@ -17,15 +17,23 @@ const scrollToBottom = () => {
 
 const ScrollButton = () => {
   return (
-    <div className="flex flex-col items-center fixed right-7 bottom-5 border rounded-xl cursour-poiner bg-rose-200 dark:bg-gray-600 dark:border-gray-400 opacity-70">
-      <ArrowUp
-        className="fill-white p-1.5 cursour-poiner border-b"
+    <div className="flex flex-col items-center fixed right-10 bottom-8 border rounded-xl cursour-poiner bg-zinc-400 dark:bg-gray-600 dark:border-gray-400 opacity-70">
+      <button
+        type="button"
+        className="p-2 border-b"
         onClick={scrollToTop}
-      />
-      <ArrowDown
-        className="fill-white p-1.5 cursour-poiner h-9"
+        aria-label="스크롤 최상단으로 이동"
+      >
+        <ArrowUp className="fill-white w-5 h-6" aria-hidden="true" />
+      </button>
+      <button
+        type="button"
+        className="p-2 border-t"
         onClick={scrollToBottom}
-      />
+        aria-label="스크롤 최하단으로 이동"
+      >
+        <ArrowDown className="fill-white w-5 h-6" aria-hidden="true" />
+      </button>
     </div>
   )
 }
