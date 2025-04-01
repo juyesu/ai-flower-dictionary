776a5f35df7d728dec1f0afd77183065e1016203
juyesu <dong4757@naver.com>
2025-04-01 20:06:58 +0900

    _document.tsx에서 _app.tsx로 <meta name="viewport" /> 위치 수정 및 dotenv.config() 서버 사이드에서만 실행되도록 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,20 +5,28 @@ import dotenv from 'dotenv'
 import Providers from '@/components/common/ThemeProvider'
 import { AuthProvider } from '@/context/AuthContext'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import Head from 'next/head'
 
 const queryClient = new QueryClient()
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  dotenv.config()
+  if (typeof window === 'undefined') {
+    dotenv.config()
+  }
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <Providers>
-          <Component {...pageProps} />
-        </Providers>
-      </AuthProvider>
-    </QueryClientProvider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <Providers>
+            <Component {...pageProps} />
+          </Providers>
+        </AuthProvider>
+      </QueryClientProvider>
+    </>
   )
 }
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,10 +5,6 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
           <link rel="icon" href="/flower.ico" />
         </Head>
         <body>
