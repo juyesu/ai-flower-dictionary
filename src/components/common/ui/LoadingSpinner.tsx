6a47c50ac376350ca63c0a7294b5ea407d7adefa
juyesu <dong4757@naver.com>
2025-04-08 02:28:39 +0900

    Loading Spinner가 header위에 표시 되지 않도록 z-index를 50 -> 40으로 수정

--- src/components/common/ui/LoadingSpinner.tsx
+++ src/components/common/ui/LoadingSpinner.tsx
@@ -1,6 +1,6 @@
 const LoadingSpinner = () => {
   return (
-    <div className="absolute left-0 top-0 z-50 flex h-full w-full flex-col items-center justify-center bg-white bg-opacity-50 dark:bg-black dark:bg-opacity-50">
+    <div className="absolute left-0 top-0 z-40 flex h-full w-full flex-col items-center justify-center bg-white bg-opacity-50 dark:bg-black dark:bg-opacity-50">
       <div className="spinner_border" />
       <p className="mt-8 dark:text-slate-200">
         결과를 생성 중입니다. <br /> 잠시만 기다려주세요
